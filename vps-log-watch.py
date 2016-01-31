2c744ba08cca08e6342e8d8c776125845d42fc1b
Alec Dhuse <alecdhuse@gmail.com>
2016-01-31 14:13:58 -0500

    Started config object

--- vps-log-watch.py
+++ vps-log-watch.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 
 import apache_tools
+import json
 import os
+
+class config:
+    log_monitors = []
+    
+    def __init__(self):
+        script_dir = os.path.dirname(__file__)
+        config_file = "logw.config"
+        file_path = os.path.join(script_dir, config_file)
+
+        if os.path.exists(file_path):
+            json_file = open(file_path)
+            json_data = json.load(json_file)
+            self.log_monitors = json_data["monitor"]; 
 
 def test_apache():
     script_dir = os.path.dirname(__file__)
@@ -10,9 +24,10 @@ def test_apache():
     log_list = apache_tools.read_apache_logfile(file_path)
 
     print log_list
-
+    
 def main():
     print "Script started"
-    test_apache()
+    config_obj = config()
+    print config_obj.log_monitors
 
 main()
