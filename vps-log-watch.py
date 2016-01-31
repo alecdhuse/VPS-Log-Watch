6613ace2b3d8dfd2d14fb49600fa2d586acde8f8
Alec Dhuse <alecdhuse@gmail.com>
2016-01-31 14:53:51 -0500

    Started process monitors function

--- vps-log-watch.py
+++ vps-log-watch.py
@@ -6,16 +6,36 @@ import os
 
 class config:
     log_monitors = []
+    file_path = ""
     
     def __init__(self):
         script_dir = os.path.dirname(__file__)
         config_file = "logw.config"
-        file_path = os.path.join(script_dir, config_file)
+        self.file_path = os.path.join(script_dir, config_file)
 
-        if os.path.exists(file_path):
-            json_file = open(file_path)
+        if os.path.exists(self.file_path):
+            json_file = open(self.file_path)
             json_data = json.load(json_file)
             self.log_monitors = json_data["monitor"]; 
+
+    def write_config(self):
+        config_json = {"monitor" : self.log_monitors}
+        
+        with open(self.file_path, 'w') as outfile:
+            json.dump(config_json, outfile)
+
+def check_monitors(config_obj):
+    for monitor in config_obj.log_monitors:
+        if monitor["type"] == "apache access combined":
+            log_list = apache_tools.read_apache_logfile(monitor["location"], monitor["last_line_read"])
+            monitor["last_line_read"] = monitor["last_line_read"] + len(log_list)
+
+    config_obj.write_config()
+    
+def main():
+    print "Script started"
+    config_obj = config()
+    check_monitors(config_obj)
 
 def test_apache():
     script_dir = os.path.dirname(__file__)
@@ -24,10 +44,5 @@ def test_apache():
     log_list = apache_tools.read_apache_logfile(file_path)
 
     print log_list
-    
-def main():
-    print "Script started"
-    config_obj = config()
-    print config_obj.log_monitors
 
 main()
